a58519b6a50f333ef38e83bd48f86350c9742eed
erickmolocho0-lang <erickmolocho0@gmail.com>
2026-06-26 13:32:54 -0500

    ërror404

--- Config/Config.php
+++ Config/Config.php
@@ -1,12 +1,19 @@
 <?php
 
-const BASE_URL = "http://localhost/semana3/";
+// Base URL dinámica: funciona sin importar en qué carpeta esté el proyecto
+function base_url() {
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+    $host     = $_SERVER['HTTP_HOST'];
+    $script   = dirname($_SERVER['SCRIPT_NAME']);
+    $base     = rtrim($script, '/\\');
+    return $protocol . '://' . $host . $base . '/';
+}
 
-const CONNECTION=false;
-const DB_HOST = "localhost";
-const DB_NAME = "proyecto_php";
-const DB_USER = "root";
-const DB_PASS= "";
-const DB_CHARSET = "utf8";
+const BASE_URL = "";   // Mantenido por compatibilidad; usar base_url() en su lugar
 
-?>
+const CONNECTION  = false;
+const DB_HOST     = "localhost";
+const DB_NAME     = "proyecto_php";
+const DB_USER     = "root";
+const DB_PASS     = "";
+const DB_CHARSET  = "utf8";
